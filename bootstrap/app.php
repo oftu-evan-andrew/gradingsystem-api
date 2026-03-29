06d3f3f288c144f01024f0bcd0d5eb112ce86df7
oftu-evan-andrew <107543784+oftu-evan-andrew@users.noreply.github.com>
2026-03-29 14:56:23 +0800

    fix cors middleware order

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
