eda2f3ea8f9eb361fe6f26ddf2d17d61f2dd6321
oftu-evan-andrew <107543784+oftu-evan-andrew@users.noreply.github.com>
2026-04-01 03:48:31 +0800

    fix: class standing id is now nullable

--- app/Http/Requests/UpdateClassStandingRequest.php
+++ app/Http/Requests/UpdateClassStandingRequest.php
@@ -16,6 +16,8 @@ class UpdateClassStandingRequest extends FormRequest
         return [
             'type' => 'required|in:class_standing',
             'id' => 'required_without:grades|sometimes|required|exists:class_standings,id',
+            'section_subject_id' => 'sometimes|uuid|exists:section_subjects,id',
+            'grading_period' => 'sometimes|integer|between:1,3',
             'status' => 'nullable|in:draft,submitted,finalized',
             'attendance_score' => 'nullable|numeric|between:0,100',
             'recitation_score' => 'nullable|numeric|between:0,100',
@@ -23,8 +25,12 @@ class UpdateClassStandingRequest extends FormRequest
             'project_score' => 'nullable|numeric|between:0,100',
             'major_exam_pts' => 'nullable|numeric|min:0',
             'major_exam_items' => 'nullable|numeric|min:1',
+            'major_exam_score' => 'nullable|numeric|between:0,100',
             'grades' => 'sometimes|required|array|min:1',
-            'grades.*.class_standing_id' => 'required_with:grades|exists:class_standings,id',
+            'grades.*.class_standing_id' => 'nullable|uuid|exists:class_standings,id',
+            'grades.*.student_id' => 'nullable|uuid|exists:students,student_id',
+            'grades.*.section_subject_id' => 'nullable|uuid|exists:section_subjects,id',
+            'grades.*.grading_period' => 'nullable|integer|between:1,3',
             'grades.*.status' => 'nullable|in:draft,submitted,finalized',
             'grades.*.attendance_score' => 'nullable|numeric|between:0,100',
             'grades.*.recitation_score' => 'nullable|numeric|between:0,100',
@@ -32,6 +38,7 @@ class UpdateClassStandingRequest extends FormRequest
             'grades.*.project_score' => 'nullable|numeric|between:0,100',
             'grades.*.major_exam_pts' => 'nullable|numeric|min:0',
             'grades.*.major_exam_items' => 'nullable|numeric|min:1',
+            'grades.*.major_exam_score' => 'nullable|numeric|between:0,100',
         ];
     }
 }
